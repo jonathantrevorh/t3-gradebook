e9b0671dee395fe9034eb87243cddbba4c93b444
Jonathan Hunsucker <jonathantrevorh@gmail.com>
2013-12-04 03:46:58 -0500

    Move the popup to a new tab

--- events.js
+++ events.js
@@ -5,3 +5,11 @@ chrome.webRequest.onCompleted.addListener(function (details) {
     urls: ["*://t-square.gatech.edu/portal/tool/*/studentView.jsf"], 
     types: ["main_frame", "sub_frame"]
 });
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+    chrome.tabs.create({
+        "url": chrome.extension.getURL("index.html")
+    }, function(tab) {
+
+    });
+});
